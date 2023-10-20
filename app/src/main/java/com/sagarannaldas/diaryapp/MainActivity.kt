006b639ee0f42535bd6c07a32becf805651138b4
sagarannaldas <annal7057@gmail.com>
2023-10-20 16:48:59 +0530

    1. Added mongo db atlas appi id and client id in Constants 2. Navigation setup

--- app/src/main/java/com/sagarannaldas/diaryapp/MainActivity.kt
+++ app/src/main/java/com/sagarannaldas/diaryapp/MainActivity.kt
@@ -1,17 +1,25 @@
- package com.sagarannaldas.diaryapp
+package com.sagarannaldas.diaryapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.sagarannaldas.diaryapp.navigation.Screen
+import com.sagarannaldas.diaryapp.navigation.SetupNavGraph
 import com.sagarannaldas.diaryapp.ui.theme.DiaryAppTheme
 
- class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
             DiaryAppTheme {
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
---
+++ app/src/main/java/com/sagarannaldas/diaryapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.sagarannaldas.diaryapp.util
+
+object Constants  {
+    const val APP_ID = "application-0-zscsp"
+    const val CLIENT_ID = "681826234661-6358dpe8luv009k0oshf7nuuk95vjmik.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+}
